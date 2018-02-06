aa086b937884770ec9697ad46040c0d0ea3ec66d
xiaoyang <785513452@qq.com>
2018-02-06 11:16:30 +0800

    2-6

--- src/routes/project/rightPage/components/AddParams.js
+++ src/routes/project/rightPage/components/AddParams.js
@@ -5,6 +5,17 @@ import { Modal, Form, Input, Button, Select } from 'antd'
 const FormItem = Form.Item
 const Option = Select.Option
 
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 3 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 16 },
+  },
+}
+
 class AddParams extends PureComponent {
   constructor (props) {
     super(props)
@@ -13,12 +24,68 @@ class AddParams extends PureComponent {
   }
 
   render () {
+    const { getFieldDecorator } = this.props.form;
     const ModalProps = {}
     return (
       <Modal {...ModalProps}>
         <Form>
-          <FormItem>
-            
+          <FormItem
+              {...formItemLayout}
+              label="名称"
+            >
+            {
+              getFieldDecorator('name', {
+                rules: [{
+                  required: true, message: "请输入名称"
+                }],
+              })(
+                <Input placeholder="请输入名称" />
+              )
+            }
+          </FormItem>
+          <FormItem
+              {...formItemLayout}
+              label="类型"
+            >
+            {
+              getFieldDecorator('type', {
+                rules: [{
+                  required: true, message: "请选择类型"
+                }],
+              })(
+                <Select>
+                  
+                </Select>
+              )
+            }
+          </FormItem>
+          <FormItem
+              {...formItemLayout}
+              label="类型"
+            >
+            {
+              getFieldDecorator('type', {
+                rules: [{
+                  required: true, message: "请输入正确的接口名称"
+                }],
+              })(
+                <Input placeholder="请输入名称" />
+              )
+            }
+          </FormItem>
+          <FormItem
+              {...formItemLayout}
+              label="名称"
+            >
+            {
+              getFieldDecorator('name', {
+                rules: [{
+                  required: true, message: "请输入正确的接口名称"
+                }],
+              })(
+                <Input placeholder="请输入名称" />
+              )
+            }
           </FormItem>
         </Form>
       </Modal>
